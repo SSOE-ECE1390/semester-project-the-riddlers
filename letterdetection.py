0e73a35f711c55e6e5204cfa4aa0abca3b67ab20
John Klamut <jmk322@pitt.edu>
2024-12-07 12:03:29 -0500

    Inital Letter Detection using SVM

---
+++ letterdetection.py
@@ -0,0 +1,195 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import easyocr
+import pytesseract
+import mediapipe as mp
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import argparse
+from typing import Tuple, Union
+import math
+import os
+from multiprocessing.pool import ThreadPool
+from numpy.linalg import norm
+from common import clock, mosaic
+
+SZ = 20 # size of each digit is SZ x SZ
+CLASS_N = 10
+DIGITS_FN = "digits.png"
+
+def split2d(img, cell_size, flatten=True):
+    h, w = img.shape[:2]
+    sx, sy = cell_size
+    cells = [np.hsplit(row, w//sx) for row in np.vsplit(img, h//sy)]
+    cells = np.array(cells)
+    if flatten:
+        cells = cells.reshape(-1, sy, sx)
+    return cells
+
+def load_digits(fn):
+    #fn = cv.samples.findFile(fn)
+    print('loading "%s" ...' % fn)
+    digits_img = cv2.imread(fn, cv2.IMREAD_GRAYSCALE)
+    digits = split2d(digits_img, (SZ, SZ))
+    labels = np.repeat(np.arange(CLASS_N), len(digits)/CLASS_N)
+    return digits, labels
+
+def deskew(img):
+    m = cv2.moments(img)
+    if abs(m['mu02']) < 1e-2:
+        return img.copy()
+    skew = m['mu11']/m['mu02']
+    M = np.float32([[1, skew, -0.5*SZ*skew], [0, 1, 0]])
+    img = cv2.warpAffine(img, M, (SZ, SZ), flags=cv2.WARP_INVERSE_MAP | cv2.INTER_LINEAR)
+    return img
+
+
+class KNearest(object):
+    def __init__(self, k = 3):
+        self.k = k
+        self.model = cv2.ml.KNearest_create()
+
+    def train(self, samples, responses):
+        self.model.train(samples, cv2.ml.ROW_SAMPLE, responses)
+
+    def predict(self, samples):
+        _retval, results, _neigh_resp, _dists = self.model.findNearest(samples, self.k)
+        return results.ravel()
+
+    def load(self, fn):
+        self.model = cv2.ml.KNearest_load(fn)
+
+    def save(self, fn):
+        self.model.save(fn)
+
+class SVM(object):
+    def __init__(self, C = 1, gamma = 0.5):
+        self.model = cv2.ml.SVM_create()
+        self.model.setGamma(gamma)
+        self.model.setC(C)
+        self.model.setKernel(cv2.ml.SVM_RBF)
+        self.model.setType(cv2.ml.SVM_C_SVC)
+
+    def train(self, samples, responses):
+        self.model.train(samples, cv2.ml.ROW_SAMPLE, responses)
+
+    def predict(self, samples):
+        return self.model.predict(samples)[1].ravel()
+
+    def load(self, fn):
+        self.model = cv2.ml.SVM_load(fn)
+
+    def save(self, fn):
+        self.model.save(fn)
+        
+
+def evaluate_model(model, digits, samples, labels):
+    resp = model.predict(samples)
+    err = (labels != resp).mean()
+    print('error: %.2f %%' % (err*100))
+
+    confusion = np.zeros((10, 10), np.int32)
+    for i, j in zip(labels, resp):
+        confusion[i, int(j)] += 1
+    print('confusion matrix:')
+    print(confusion)
+    print()
+
+    vis = []
+    for img, flag in zip(digits, resp == labels):
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        if not flag:
+            img[...,:2] = 0
+        vis.append(img)
+    return mosaic(25, vis)
+
+def preprocess_simple(digits):
+    return np.float32(digits).reshape(-1, SZ*SZ) / 255.0
+
+def preprocess_hog(digits):
+    samples = []
+    for img in digits:
+        gx = cv2.Sobel(img, cv2.CV_32F, 1, 0)
+        gy = cv2.Sobel(img, cv2.CV_32F, 0, 1)
+        mag, ang = cv2.cartToPolar(gx, gy)
+        bin_n = 16
+        bin = np.int32(bin_n*ang/(2*np.pi))
+        bin_cells = bin[:10,:10], bin[10:,:10], bin[:10,10:], bin[10:,10:]
+        mag_cells = mag[:10,:10], mag[10:,:10], mag[:10,10:], mag[10:,10:]
+        hists = [np.bincount(b.ravel(), m.ravel(), bin_n) for b, m in zip(bin_cells, mag_cells)]
+        hist = np.hstack(hists)
+
+        # transform to Hellinger kernel
+        eps = 1e-7
+        hist /= hist.sum() + eps
+        hist = np.sqrt(hist)
+        hist /= norm(hist) + eps
+
+        samples.append(hist)
+    return np.float32(samples)
+
+def predict_digit(model, img):
+
+    img = cv2.resize(img, (SZ, SZ), interpolation=cv2.INTER_AREA)
+    img = deskew(img)
+    plt.imshow(img, cmap='gray')
+    plt.title("Deskewed Image")
+    plt.show()
+    sample = preprocess_hog([img])
+    response = model.predict(sample)
+    return int(response[0])
+
+
+def train(model):
+
+    digits, labels = load_digits(DIGITS_FN)
+
+    print('preprocessing...')
+    # shuffle digits
+    rand = np.random.RandomState(321)
+    shuffle = rand.permutation(len(digits))
+    digits, labels = digits[shuffle], labels[shuffle]
+
+    digits2 = list(map(deskew, digits))
+    samples = preprocess_hog(digits2)
+
+    train_n = int(0.9*len(samples))
+    digits_train, digits_test = np.split(digits2, [train_n])
+    samples_train, samples_test = np.split(samples, [train_n])
+    labels_train, labels_test = np.split(labels, [train_n])
+
+
+    print('training SVM...')
+    svm_model = SVM(C=2.67, gamma=5.383)
+    svm_model.train(samples_train, labels_train)
+    vis = evaluate_model(svm_model, digits_test, samples_test, labels_test)
+    svm_model.save('digits_svm.dat')
+    
+    print('training KNearest...')
+    kn_model = KNearest(k=4)
+    kn_model.train(samples_train, labels_train)
+    vis = evaluate_model(kn_model, digits_test, samples_test, labels_test)
+    
+    if model:
+        return kn_model
+    else:
+        return svm_model
+
+
+def decode(image, model):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    digit = predict_digit(model, gray)
+    plt.imshow(gray, cmap='gray')
+    plt.title("Digit Region")
+    plt.show()
+    
+    return digit
+
+
+
+if __name__ == '__main__':
+    print("Hello World")
